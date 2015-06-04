5afc45863f5a4a839734b982c85faafcc31f7989
PA Elom Worlanyo <paelomworlanyo@pa-d-students05-p.dhcp.wustl.edu>
2015-06-04 14:50:35 -0500

    working out cronJob

--- scripts/kanbanize.js
+++ scripts/kanbanize.js
@@ -25,7 +25,7 @@ module.exports = function(robot) {
         }
 
         this.apikey = null != options.apikey ? options.apikey : kanEnum.API_KEY;
-        domain = null != options.domain ? options.domain + '.' : " ";
+        domain = null != options.domain ? options.domain + '.' : kanEnum.BASE_DOMAIN + '.';
         this.kanbanize_url = "http://" + domain + kanEnum.BASE_URL;
     }
 
@@ -54,7 +54,11 @@ module.exports = function(robot) {
             if (xmlhttp.readyState === 4) {
                 if (xmlhttp.status === 200) {
                     response = xmlhttp.responseText;
-                    return res.reply(response);
+                    var parser = require('xml2json');
+                    var json = JSON.parse(parser.toJson(response));
+                    var comments = json.xml.activities.item;
+                    //console.log(comments[0]);
+                    return res.reply(JSON.stringify(comments));
                 } else if(xmlhttp.status === 400) {
                     return res.reply('No comments in the last hour')
                 }else {
@@ -84,35 +88,40 @@ module.exports = function(robot) {
     KanbanizeJS.prototype.date_repr = function(date){
         var newdate = date.getFullYear() + "-"  
                         + (date.getMonth() + 1)  + "-" 
-                        + date.getDate() + " "
-                        + date.getHours() + ":"  
-                        + date.getMinutes() + ":" 
-                        + date.getSeconds();
+                        + date.getDate()// + " "
+                        //+ date.getHours() + ":"  
+                        //+ date.getMinutes() + ":" 
+                        //+ date.getSeconds();
             return newdate;
     };
 	
 	robot.respond(/any new comments\s?\?/i, function(res){
 
-		var kanbanize = new KanbanizeJS({
-            apikey : kanEnum.API_KEY,
-        });
+		var cron = require('cron');
+        var cronJob = cron.job("0 */10 * * * *", function(){
+            var kanbanize = new KanbanizeJS({
+                apikey : kanEnum.API_KEY,
+            });
 
-        var today = new Date().getTime(); 
-        var endtime = kanbanize.date_repr(new Date(today + (1000*60*60*24)));
-        var hourago = kanbanize.date_repr(new Date(today - (1000*60*60)));
+            var today = new Date().getTime(); 
+            var fromtime = kanbanize.date_repr(new Date(today));
+            var endtime = kanbanize.date_repr(new Date(today + (1000*60*60*24)));
 
-        get_board_activities = {
-            "function": 'get_board_activities',
-            data:{
-                boardid : '9',
-                fromdate : hourago,
-                todate : endtime,
-                history : 'yes',
-                eventtype : 'Comments',
+            get_board_activities = {
+                "function": 'get_board_activities',
+                data:{
+                    boardid : '9',
+                    fromdate : fromtime,
+                    todate : endtime,
+                    history : 'yes',
+                    eventtype : 'Comments',
+                }
             }
-        }
 
-        kanbanize.call(get_board_activities, res);
+            kanbanize.call(get_board_activities, res);
+        }); 
+        cronJob.start();
+
 	});
 
 
