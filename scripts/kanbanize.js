79a9a5d9073d370bbc3a398d976acc8389bd11a9
PA Elom Worlanyo <paelomworlanyo@pa-d-students05-p.dhcp.wustl.edu>
2015-06-05 14:40:51 -0500

    Streamlining

--- scripts/kanbanize.js
+++ scripts/kanbanize.js
@@ -6,7 +6,7 @@
 //   xml2json
 //
 // Configuration:
-//   Should have a config.json file containing api key and base domain
+//   Should have a config.json file containing board id, api key and base domain
 //
 // Commands:
 //	 wubot any new comments ? - returns any new comments on kanbanize cards
@@ -16,16 +16,9 @@ module.exports = function(robot) {
 
     var kanEnum = require('../config.json');
 
-    var KanbanizeJS = function(options) {
-        var domain;
-        if (null != options.email && null != options.password){
-            this.email = options.email;
-            this.email = options.email;
-            this.password = options.password;
-        }
-
+    var KanbanizeJS = function(options) {        
         this.apikey = null != options.apikey ? options.apikey : kanEnum.API_KEY;
-        domain = null != options.domain ? options.domain + '.' : kanEnum.BASE_DOMAIN + '.';
+        var domain = null != options.domain ? options.domain + '.' : kanEnum.BASE_DOMAIN + '.';
         this.kanbanize_url = "http://" + domain + kanEnum.BASE_URL;
     }
 
@@ -41,11 +34,7 @@ module.exports = function(robot) {
     };
 
     KanbanizeJS.prototype.call = function(apiCall, res) {
-        var l, url, xmlhttp, response;
-        if (apiCall["function"] !== 'login' && (this.apikey == null)) {
-            l = this.login();
-            this.apikey = l.apikey;
-        }
+        var url, xmlhttp, response;
         url = this._getUrl(apiCall);
         var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
         xmlhttp = new XMLHttpRequest();
@@ -55,7 +44,10 @@ module.exports = function(robot) {
                 if (xmlhttp.status === 200) {
                     response = xmlhttp.responseText;
                     var parser = require('xml2json');
-                    var json = JSON.parse(parser.toJson(response));
+                    var options =   { 
+                                        "sanitized" : "true"
+                                    }
+                    var json = JSON.parse(parser.toJson(response, options));
                     var comments = json.xml.activities.item;
                     //console.log(comments[0]);
                     return res.reply(JSON.stringify(comments));
@@ -73,26 +65,11 @@ module.exports = function(robot) {
         xmlhttp.send();        
     };
 
-    KanbanizeJS.prototype.login = function() {
-        var call;
-        call = {
-        "function": 'login',
-        data: {
-          email: this.email,
-          password: this.password
-        }
-        };
-        return this.call(call);
-    };
-
     KanbanizeJS.prototype.date_repr = function(date){
         var newdate = date.getFullYear() + "-"  
                         + (date.getMonth() + 1)  + "-" 
-                        + date.getDate()// + " "
-                        //+ date.getHours() + ":"  
-                        //+ date.getMinutes() + ":" 
-                        //+ date.getSeconds();
-            return newdate;
+                        + date.getDate();
+        return newdate;
     };
 	
 	robot.respond(/any new comments\s?\?/i, function(res){
@@ -110,7 +87,7 @@ module.exports = function(robot) {
             get_board_activities = {
                 "function": 'get_board_activities',
                 data:{
-                    boardid : '9',
+                    boardid : kanEnum.BOARD_ID,
                     fromdate : fromtime,
                     todate : endtime,
                     history : 'yes',
