6e6421359441b6046df166be35d84afed9c698bf
PA Elom Worlanyo <paelomworlanyo@pa-d-students05-p.dhcp.wustl.edu>
2015-07-20 15:18:35 -0500

    More clean up found and completed

--- scripts/kanbanize-comments.js
+++ scripts/kanbanize-comments.js
@@ -14,34 +14,36 @@
 //  KANBANIZE_BOARD_ID - specify which Kanban board to check
 //
 // Commands:
-//	 wubot any new comments ? - returns any new comments on kanbanize cards
- 
-
+//   wubot any new comments ? - returns any new comments on kanbanize cards
 module.exports = function(robot) {
 
     /*Load node-cron module to enable cron job to run*/
     var cron = require('cron');
 
+    /*Time constants*/
+    var day = 1000 * 60 * 60 * 24;
+    var fifteenMinutes = 1000 * 60 * 15; 
+    var weekendHours = 1000 * 60 * 60 * 61;
+
     /*Simple function to format date in manner acceptable by Kanbanize API*/
-    var kanbanizeDate = function(date){
-        var newDate = date.getFullYear() + "-"  
-                    + (date.getMonth() + 1)  + "-" 
+    var kanbanizeDate = function(date) {
+        var newDate = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" 
                     + date.getDate();
         return newDate;
     };
-    
+
     /*Function to call one of the methods provided by the Kanbanize API*/
-    var callKanbanize = function(apiCall, callback){
-        var command_url =  process.env.KANBANIZE_API_URL + apiCall.command;
+    var callKanbanize = function(apiCall, callback) {
+        var command_url = process.env.KANBANIZE_API_URL + apiCall.command;
         robot.http(command_url)
             .header("apikey", process.env.KANBANIZE_API_KEY)
             .header("Accept", "application/json")
             .post(JSON.stringify(apiCall.parameters))
-            (function(err, res, body){
-                try{
+            (function(err, res, body) {
+                try {
                     response = JSON.parse(body);
                     callback(response);
-                }catch(err){
+                } catch (err) {
                     console.log(err);
                 }
             });
@@ -51,61 +53,57 @@ module.exports = function(robot) {
         what channel said comment should be posted in. Assumes that the lane
         name and channel name are analogous. Can link the lane name and channel
         name using a json file if necessary*/
-    var displayToLane = function(comment){
+    var displayToLane = function(comment) {
         var channels = {
-                            "TAB" : "tab",
-                            "Bjorn" : "pod-bjorn",
-                            "Pod Squad" : "pod-squad"
+            "TAB": "tab",
+            "Bjorn": "pod-bjorn",
+            "Pod Squad": "pod-squad"
         };
 
         var taskID = comment.taskid;
         var task_data = JSON.stringify({
-                            command: "get_task_details",
-                            parameters:{
-                                boardid : process.env.KANBANIZE_BOARD_ID,
-                                taskid:taskID,
-                                format: "json",
-                            }
-                        });
+            command: "get_task_details",
+            parameters: {
+                boardid: process.env.KANBANIZE_BOARD_ID,
+                taskid: taskID,
+                format: "json",
+            }
+        });
         var apiCall = JSON.parse(task_data);
-        callKanbanize(apiCall, function(response){
+        callKanbanize(apiCall, function(response) {
             var room = channels[response.lanename];
             var color = response.color;
-            console.log(room);
             displayComment(comment, room, color);
         });
     };
 
     /*Function to format and display any obtained comment in given Slack room*/
-    var displayComment = function(comment, room, commentColor){
+    var displayComment = function(comment, room, commentColor) {
         link = "https://wustlpa.kanbanize.com/ctrl_board/9/" + comment.taskid;
         var msg = {
-                  message: {
-                    reply_to: "general",
-                    room: room
-                     }
-                };
+            message: {
+                reply_to: "general",
+                room: room
+            }
+        };
 
         var content = {
-          text: '',
-          fallback: comment.author + " *added a comment* on `" 
-                    + comment.taskid + "`: \"" + comment.text 
-                    + "\" \n\n" + link,
-          pretext: comment.event + " by *" + comment.author + "*\n",
-          color: commentColor,
-          mrkdwn_in: ["pretext", "title", "fallback", "fields"],
-          fields: [
-            {
+            text: '',
+            fallback: comment.author + " *added a comment* on `" 
+                        + comment.taskid + "`: \"" + comment.text 
+                        + "\" \n\n" + link,
+            pretext: comment.event + " by *" + comment.author + "*\n",
+            color: commentColor,
+            mrkdwn_in: ["pretext", "title", "fallback", "fields"],
+            fields: [{
                 title: "Comment",
                 value: comment.text,
                 short: true
-            }, 
-            {
+            }, {
                 title: "Task ID",
                 value: "<" + link + "|" + comment.taskid + ">",
                 short: true
-            }
-          ]
+            }]
         };
 
         msg.content = content;
@@ -113,67 +111,62 @@ module.exports = function(robot) {
     };
 
     /*Function to get all comments made from a given time to now*/
-    var getComments = function(time){
-        //How far back to query api
-        var fromTime = null != time? new Date(Date.now() - time): new Date();
+    var getComments = function(time) {
+        /*How far back to query api*/
+        var fromTime = null != time ? new Date(Date.now() - time) : new Date();
         var from = kanbanizeDate(fromTime);
-        console.log(fromTime);
-        console.log(from);
-        //Next day; ensures all comments include current day.
-        var end = kanbanizeDate(new Date(Date.now() + (1000*60*60*24))); 
-        
+        /*Next day; ensures all comments include current day.*/
+        var end = kanbanizeDate(new Date(Date.now() + day);
+
         /*required parameters to retrieve comments from Kanbanize api*/
         var board_data = JSON.stringify({
-                            command: "get_board_activities",
-                            parameters:{
-                                boardid : process.env.KANBANIZE_BOARD_ID,
-                                fromdate : from,
-                                todate : end,
-                                history : "yes",
-                                eventtype : "Comments",
-                                format: "json",
-                            }
-                        });
+            command: "get_board_activities",
+            parameters: {
+                boardid: process.env.KANBANIZE_BOARD_ID,
+                fromdate: from,
+                todate: end,
+                history: "yes",
+                eventtype: "Comments",
+                format: "json",
+            }
+        });
         var apiCall = JSON.parse(board_data);
-        callKanbanize(apiCall, function(response){
-            console.log(response);
+        callKanbanize(apiCall, function(response) {
             var comments = response.activities;
-            var earliestTime = new Date(Date.now() - (1000*60*60*15))  //default
+            var earliestTime = new Date(Date.now() - fifteenMinutes) //default
 
-            if(null != time){
+            if (null != time) {
                 earliestTime = new Date(Date.now() - time);
             }
 
             /*Display any comments that were made within specified time*/
-            for(var i = 0; i < comments.length; i++){
+            for (var i = 0; i < comments.length; i++) {
                 comment = comments[i];
                 commentTime = new Date(comment.date);
-                if (commentTime.getTime() >= earliestTime.getTime()){
+                if (commentTime.getTime() >= earliestTime.getTime()) {
                     displayToLane(comment);
                 }
             }
         });
     };
-    
-	/*Returns any new comments added within the last 15 minutes
+
+    /*Returns any new comments added within the last 15 minutes
         during the working week.*/
-    var dailyCronJob = cron.job("*/15 06-16 * * 1-5", function(){
-        getComments(1000*60*15); //every 15 minutes
-        console.log("Running daily cron");
-    }); 
+    var dailyCronJob = cron.job("*/15 06-16 * * 1-5", function() {
+        getComments(fifteenMinutes); //every 15 minutes
+    });
     dailyCronJob.start();
 
     /*Returns any new comments added over the weekend*/
-    var weeklyCronJob = cron.job("00 06 * * 1", function(){
-        getComments(1000*60*60*61); //every 61 hours. (from 5pm fri - 6am mon)
-        console.log("Running weekly cron");
-    }); 
+    var weeklyCronJob = cron.job("00 06 * * 1", function() {
+        getComments(weekendHours); //every 61 hours. (from 5pm fri - 6am mon)
+    });
     weeklyCronJob.start();
 
     //One-off reply to any user query for any new comments
-	robot.respond(/any new comments\s?\?/i, function(res){
+    robot.respond(/any new comments\s?\?/i, function(res) {
         getComments();
-	});
+    });
 
 
-} 
+}
